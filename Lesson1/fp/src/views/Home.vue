5ce4eb3bf305a9683291802598de39b0e30bd4f9
Ruslan Borodin <BorodinRA@GMail.com>
2021-10-19 16:49:41 +0300

    lesson1

---
+++ Lesson1/fp/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!--<img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <geek-calculator />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GeekCalculator from "@/components/Calculator.vue";
+
+export default {
+  name: "Home",
+  components: {
+    GeekCalculator,
+  },
+};
+</script>
